35188ffe5f3c0036fe5b48ab261f54af4fbcfb29
Glomay <maiyittegloria@live.fr>
2017-07-26 16:36:55 +0100

    Premier commit
    Envoi des premiers fichiers

---
+++ header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PlayerGame</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font-Awesome -->
+    <link href="fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- open-sans -->
+    <link href="fonts/open-sans/stylesheet.css" rel="stylesheet">
+    <!-- Sf cartoonist hand -->
+    <link href="fonts/sf_cartoonist_hand/stylesheet.css" rel="stylesheet">
+    <!-- Silkscreen -->
+    <link href="fonts/Silkscreen/stylesheet.css" rel="stylesheet">
+    <!-- OwlCarousel -->
+    <link href="plugins/owlCarousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="plugins/owlCarousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
+    <!-- Google Fonts -->
+    <!--<link href="https://fonts.googleapis.com/css?family=Fjalla+One|Roboto" rel="stylesheet">-->
+    <!-- Animate-css -->
+    <link rel="stylesheet" href="plugins/animate-css/animate.css">
+    <!-- css file -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.png">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page
+    via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/
+html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/
+respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<header class="main-header">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <a href="#" id="main-logo1"> <img src="img-playergame/logo4.png" height="70"> </a>
+            </div>
+            <div class="col-sm-6">
+                <div class="nav-menu">
+                    <nav class="navbar" role="navigation">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                    data-target="#example-navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <!--<a class="navbar-brand" href="#">TutorialsPoint</a>-->
+                        </div>
+                        <div class="collapse navbar-collapse" id="example-navbar-collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Accueil</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                        Nos Activites <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Libreville Gaming</a></li>
+                                        <li><a href="#">Biss Ngabu</a></li>
+                                        <li><a href="#">School Chalenge</a></li>
+                                        <li><a href="#">Solialy</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Mediatheque</a></li>
+                                <li><a href="#">Xchanges</a></li>
+                                <li><a href="#">Calendrier</a></li>
+                                <li><a href="#contacts" class="anchor">Contacts</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            
+            <div class="col-sm-3">
+                <ul class="social-media">
+                    <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="#" target="_blank"><i class="fa fa-youtube"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,141 @@
+<?php include "header.php"?>
+
+<div id="slide1">
+    <div class="content">
+        <div class="container">
+            <h1>LIBREVILLE GAMING</h1>
+            <p>Du 10 au 11 Aout 2017</p>
+            <button class="submit" type="submit">En savoir plus</button>
+        </div>
+    </div>
+</div>
+
+<div id="slide2">
+    <div class="content">
+        <div class="container">
+            <p>PLAYERGAME est une plateforme de jeu virtuel sur tous supports de jeux-vidéos. Concept mise en place pour créer une communauté gabonaise des fanatiques de cette passion autours des championnats et tournois des différents jeux.
+                Les objectifs visés à travers PlayerGame sont non seulement axés sur un type de divertissement
+                responsable mais surtout sur l’idée d’apprendre à devenir des acteurs aussi bien passifs qu’actifs
+                de ce milieu.</p>
+        </div>
+    </div>
+</div>
+
+<div id="slide3">
+    <div class="content">
+        <div class="container">
+            <h2>Nos activites</h2>
+            <div class="col-sm-3">
+                <p>Libreville Gaming</p>
+                <div class="frame">
+                    <img src="img-playergame/web5.jpg" alt="" class="img-responsive">
+                    <span></span>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <p>Biss Ngabu</p>
+                <div class="frame">
+                    <img src="img-playergame/web6.jpg" alt="" class="img-responsive">
+                    <span></span>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <p>School Challenge</p>
+                <div class="frame">
+                    <img src="img-playergame/web7.jpg" alt="" class="img-responsive">
+                    <span></span>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <p>Socialy</p>
+                <div class="frame">
+                    <img src="img-playergame/web8.jpg" alt="" class="img-responsive">
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="slide4">
+    <div class="content">
+        <div class="container">
+            <h2>Calendrier</h2>
+            <div class="col-sm-4 zoom">
+                <div class="calendrier">
+                    <a href="#" id="photo"> <img src="img-playergame/pg2.jpg" class="img-responsive"> </a>
+                    <h5>10/05/2017</h5>
+                    <h4>Interdum et malesuada fames</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem massa, pellentesque ut facilisis nec, vulputate vitae tortor. Suspendisse id iaculis neque. Praesent non turpis sed neque sodales egestas. Sed ut dui id sem sodales auctor eu tempor augue.</p>
+                    <button class="submit" type="submit">En savoir plus</button>
+                </div>
+            </div>
+            <div class="col-sm-4 zoom">
+                <div class="calendrier">
+                    <a href="#" id="photo"> <img src="img-playergame/pg3.jpg" class="img-responsive"> </a>
+                    <h5>10/05/2017</h5>
+                    <h4>Interdum et malesuada fames</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem massa, pellentesque ut facilisis nec, vulputate vitae tortor. Suspendisse id iaculis neque. Praesent non turpis sed neque sodales egestas. Sed ut dui id sem sodales auctor eu tempor augue.</p>
+                    <button class="submit" type="submit">En savoir plus</button>
+                </div>
+            </div>
+            <div class="col-sm-4 zoom">
+                <div class="calendrier">
+                    <a href="#" id="photo"> <img src="img-playergame/pg4.jpg" class="img-responsive"> </a>
+                    <h5>10/05/2017</h5>
+                    <h4>Interdum et malesuada fames</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem massa, pellentesque ut facilisis nec, vulputate vitae tortor. Suspendisse id iaculis neque. Praesent non turpis sed neque sodales egestas. Sed ut dui id sem sodales auctor eu tempor augue.</p>
+                    <button class="submit" type="submit">En savoir plus</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <section id="slide5" class="section-gallery">
+        <div class="container-fluid">
+            <div class="galerie">
+                <a href="#">
+                    <h2>PlayerGame en images</h2>
+                    <span>Toutes les photos ›</span>
+                </a>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="img-playergame/pg6.jpg" alt="" class="img-responsive">
+                </div>
+                <div class="col-sm-6">
+                    <div class="row">
+                        <div class="media col-sm-6">
+                            <img src="img-playergame/pg7.jpg" alt="" class="img-responsive">
+                        </div>
+                        <div class="media col-sm-6">
+                            <img src="img-playergame/pg7.jpg" alt="" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <img src="img-playergame/pg7.jpg" alt="" class="img-responsive">
+                        </div>
+                        <div class="col-sm-6">
+                            <img src="img-playergame/pg7.jpg" alt="" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div id="slide6">
+        <div class="content">
+            <div class="container">
+                <h2>Xchanges</h2>
+                <p>Votre espace d'exposition,de vente,de promotion de vos produits et services dans le domaine du digital et du numerique.</p>
+                <a href="#"><i class="fa fa-long-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+
+
+
+<?php include "footer.php"?>
